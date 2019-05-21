496d145de657c0b5f8ab13dcec3aecf91156861e
Jacob Allen <boat.mon@gmail.com>
2019-05-21 01:10:42 -0400

    fixed header styling bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ class Home extends React.Component {
  render() {
    return (
    <div className="master">
-    <header/>
+    <header className="header"/>
       <div className="top">
       </div>
 
