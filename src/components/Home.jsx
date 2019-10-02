1f1db1af091b84776d9bf9b9ed47162c5448a97f
Jacob Allen <boat.mon@gmail.com>
2019-10-02 13:37:03 -0400

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -115,7 +115,7 @@ class Home extends React.Component {
             <a href="https://beat-blender.herokuapp.com/">
               <img src="images/BeatBleader.png" alt="" width="800" height="400"/>
             </a>
-            <div className="desc">BeatBleader uses Geolocation and the Spotify API to connect you with other people around you through music.</div>
+            <div className="desc">BeatBleader uses Geolocation and the Spotify API to connect you with other people around you through music. It was my teams final project at Wyncode in 2019</div>
           </div>
         </div>
         <div className="responsive">
@@ -123,7 +123,15 @@ class Home extends React.Component {
             <a href="https://github.com/Jacobaallen/Weekend-Game-2">
               <img src="images/game.png" alt="game" width="800" height="400"/>
             </a>
-            <div className="desc">Elder Tales is a game made for the terminal based on the anime "Log Horizon".</div>
+            <div className="desc">Elder Tales is a game made for the terminal based on the anime "Log Horizon". I created in 2019.</div>
+          </div>
+        </div>
+        <div className="responsive">
+          <div className="gallery">
+            <a href="http://www.animefanhelper.com">
+              <img src="images/AnimeFanHelper.png" alt="AnimeFanHelper" width="800" height="400"/>
+            </a>
+            <div className="desc">Anime Fan Helper is a website that helps to find information about anime. Using the Jikan API. Created in 2019.</div>
           </div>
         </div>
       </div>
@@ -133,15 +141,23 @@ class Home extends React.Component {
             <a href="https://tourdeendofsummer.herokuapp.com/index.html">
               <img src="images/Tourdeendofsummer.png" alt="TourDeEndofSummer" width="800" height="400"/>
             </a>
-            <div className="desc">Tour De End Of Summer is an event site to find out information about a bike ride.</div>
+            <div className="desc">A event sited I create using Adobe Dreamweaver in 2016.</div>
           </div>
         </div>
         <div className="responsive">
           <div className="gallery">
-            <a href="http://www.animefanhelper.com">
-              <img src="images/AnimeFanHelper.png" alt="AnimeFanHelper" width="800" height="400"/>
+            <a href="http://garyramey.com/student-work/gra2144c-sp2016/jallen/project2/index.html">
+              <img src="images/ecommerce-site.png" alt="JaKob" width="800" height="400"/>
             </a>
-            <div className="desc">Anime Fan Helper is a website that helps to find information about anime. Using the Jikan API.</div>
+            <div className="desc">A mock up ecommerce site I created using Adobe Muse in 2016.</div>
+          </div>
+        </div>
+        <div className="responsive">
+          <div className="gallery">
+            <a href="http://garyramey.com/student-work/gra2144c-sp2016/jallen/project1/index.html">
+              <img src="images/event-site.png" alt="event" width="800" height="400"/>
+            </a>
+            <div className="desc">A mock up event site I created using Adobe Muse in 2016.</div>
           </div>
         </div>
       </div>
@@ -151,7 +167,7 @@ class Home extends React.Component {
             <a href="http://www.animefanhelper.com/whackaluffy.html">
               <img src="images/whackaluffy0.png" alt="whackaluffy" width="800" height="400"/>
             </a>
-            <div className="desc">Based on the anime One Piece and the game Whack-a-mole comes Whack-a-Luffy.</div>
+            <div className="desc">Based on the anime One Piece and the game Whack-a-mole comes Whack-a-Luffy. Created in 2019.</div>
           </div>
         </div>
         <div className="responsive">
@@ -159,7 +175,7 @@ class Home extends React.Component {
             <a href="http://www.animefanhelper.com/space-invaders.html">
               <img src="images/arcade-game-si.png" alt="Space-invaders" width="800" height="400"/>
             </a>
-            <div className="desc">Based on the most game popular ever made.</div>
+            <div className="desc">Based on the most game popular ever made. Created in 2019.</div>
           </div>
         </div>
         <div className="responsive">
@@ -167,7 +183,7 @@ class Home extends React.Component {
             <a href="http://www.animefanhelper.com/cowboybebopspacebattle.html">
               <img src="images/arcade-game-CBSB.png" alt="Cowboy-Bebop" width="800" height="400"/>
             </a>
-            <div className="desc">Based on the anime Cowboy Bebop comes a first person shooter game.</div>
+            <div className="desc">Based on the anime Cowboy Bebop comes a first person shooter game. Created in 2019.</div>
           </div>
         </div>
       </div>
