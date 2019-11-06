aa3601e72a2c0e296b8e5d18669d6b852aa413d3
Jacob Allen <boat.mon@gmail.com>
2019-11-06 02:24:31 -0500

    added the new componet and coming soon

---
+++ src/components/Art.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/art.css';
+
+
+class Art extends React.Component {
+ render() {
+   return (
+     <div className="coming-soon">
+      <h1>Coming Soon</h1>
+    </div>
+   )
+ }
+}
+
+export default Art;
